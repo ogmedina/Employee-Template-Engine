98ea00b829044dc10d13990a23e6a687134cd5d2
Oscar Medina <ogmedina04@gmail.com>
2020-12-22 13:07:08 -0600

    added script files for different roles

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//Brings in file information from script files
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+//Required modules for program to run
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const employeeObjects = [];
+//Output Directory file and path. This goes to the output folder, and creates an html called team
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -60,7 +64,31 @@ inquirer.prompt([
 
 newManager();
 
+const newIntern = () =>
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your intern\'s name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your intern\s id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is your intern\'s email?',
+        name: 'email',   
+    },
+    {
+        type: 'input',
+        message: 'What is your intern\'s school?',
+        name: 'school'        
+    }
 
+
+])
 
 
 const render = require("./lib/htmlRenderer");
